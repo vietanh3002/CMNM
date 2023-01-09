4e35324656ab4607c9b861a6f4ebccfd0462c68a
vietanh3002 <118631118+vietanh3002@users.noreply.github.com>
2023-01-09 09:55:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var fut = new Date("jan 6, 2023 08:40:20").getTime()
+var fut = new Date("jan 22, 2023 00:00:00").getTime()
 let x = setInterval(function stime(){
     var now = new Date().getTime()
     var D = fut - now
